fd6c49cca35449f53c237874776bb8ae9db0d740
Matthew Soulanille <matthew@soulanille.net>
2019-05-09 23:33:06 -0400

    Draw your own polygons

--- src/visualization/browser.ts
+++ src/visualization/browser.ts
@@ -1,5 +1,6 @@
 import * as PIXI from "pixi.js";
 import { MakePolygon } from "./DrawPolygon";
+import { Polygon } from "../PolygonTypes";
 
 
 const app = new PIXI.Application({
@@ -11,18 +12,26 @@ const app = new PIXI.Application({
 
 document.body.appendChild(app.view);
 
+app.stage.interactive = true;
 
 const g = new PIXI.Graphics();
-
-g.position.x = window.innerWidth / 2;
-g.position.y = window.innerHeight / 2;
-
 app.stage.addChild(g);
 
-g.lineStyle(4, 0xff0000);
 
-let poly1 = MakePolygon([[10, 10], [90, 30], [45, 45]]);
+
+
+document.addEventListener('mouseup', onMouseUp);
+
+
+let poly1: Polygon = [];
+var pixiPoly1: PIXI.Polygon;
+function onMouseUp(event: MouseEvent) {
+    g.clear()
+    g.lineStyle(4, 0xff0000);
+    poly1.push([event.clientX, event.clientY]);
+    pixiPoly1 = MakePolygon(poly1);
+    g.drawPolygon(pixiPoly1);
+}
 
 (window as any).poly1 = poly1;
 (window as any).g = g;
-g.drawPolygon(poly1);
